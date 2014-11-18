49a8decf296ca393b73a0c3f5f236bae911e1bd1
Mathieu Schroeter <mathieu@schroetersa.ch>
2014-11-18 19:17:11 +0100

    Convert all callbackDone to callback.

--- backends/git.js
+++ backends/git.js
@@ -2,12 +2,12 @@
 
 var xProcess = require ('xcraft-core-process');
 
-exports.clone = function (uri, destPath, callbackDone) {
+exports.clone = function (uri, destPath, callback) {
   var args = [
     'clone',
     uri,
     destPath
   ];
 
-  xProcess.spawn ('git', args, callbackDone);
+  xProcess.spawn ('git', args, callback);
 };
