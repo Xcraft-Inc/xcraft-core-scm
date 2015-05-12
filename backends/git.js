6071dbbb432705be4b0588b85b6cb05a63979f81
Mathieu Schroeter <mathieu@schroetersa.ch>
2015-05-12 16:35:46 +0200

    Use a recursive git clone by default.
    It fixes the issue #117; Lydia is using at least one submodule.

--- backends/git.js
+++ backends/git.js
@@ -5,6 +5,7 @@ var xProcess = require ('xcraft-core-process') ();
 exports.clone = function (uri, destPath, callback) {
   var args = [
     'clone',
+    '--recursive',
     '--depth', '1',
     uri,
     destPath
