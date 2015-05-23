7c6358d7251f5ad74f9fe481537b738448bfd919
Mathieu Schroeter <mathieu@schroetersa.ch>
2015-05-23 15:11:43 +0200

    Add the support of refs with git backend.
    It closes #125.

--- backends/git.js
+++ backends/git.js
@@ -2,14 +2,39 @@
 
 var xProcess = require ('xcraft-core-process') ();
 
-exports.clone = function (uri, destPath, callback) {
-  var args = [
-    'clone',
-    '--recursive',
-    '--depth', '1',
-    uri,
-    destPath
-  ];
+exports.clone = function (uri, ref, destPath, callback) {
+  var async = require ('async');
+  var fs    = require ('fs');
 
-  xProcess.spawn ('git', args, {}, callback);
+  async.series ([
+    function (callback) {
+      var args = [
+        'clone',
+        '--recursive',
+        '--depth', '1',
+        uri,
+        destPath
+      ];
+
+      xProcess.spawn ('git', args, {}, callback);
+    },
+
+    function (callback) {
+      if (!fs.existsSync (destPath)) {
+        callback ('nothing cloned');
+        return;
+      }
+
+      if (!ref) {
+        ref = 'master';
+      }
+
+      var args = [
+        'checkout',
+        ref
+      ];
+
+      xProcess.spawn ('git', args, {cwd: destPath}, callback);
+    }
+  ], callback);
 };
