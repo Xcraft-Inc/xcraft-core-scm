e36d8a97229413ec3845ba6d169d8624f3d32124
Mathieu Schroeter <schroeter@epsitec.ch>
2015-06-15 15:28:46 +0200

    Use subst with the git scm backend.

--- backends/git.js
+++ backends/git.js
@@ -1,12 +1,25 @@
 'use strict';
 
 var xProcess = require ('xcraft-core-process') ();
+var xFs      = require ('xcraft-core-fs');
+var Subst    = require ('xcraft-core-subst');
 
 exports.clone = function (uri, ref, destPath, callback) {
   var async = require ('async');
   var fs    = require ('fs');
+  var path  = require ('path');
+
+  var subst = null;
+  var mountDir = path.dirname (destPath);
+
+  xFs.mkdir (mountDir);
 
   async.series ([
+    function (callback) {
+      subst = new Subst (mountDir);
+      subst.mount (callback);
+    },
+
     function (callback) {
       var args = [
         'clone',
@@ -46,6 +59,15 @@ exports.clone = function (uri, ref, destPath, callback) {
       ];
 
       xProcess.spawn ('git', args, {cwd: destPath}, callback);
+    },
+
+    function (callback) {
+      if (!subst) {
+        callback ();
+        return;
+      }
+
+      subst.umount (callback);
     }
   ], callback);
 };
