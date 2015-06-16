f0da7f71ada21f5d36445a31cf7b016846b2f8f2
Mathieu Schroeter <mathieu@schroetersa.ch>
2015-06-16 16:01:08 +0200

    Simplify the git backend with subst.wrap.

--- backends/git.js
+++ backends/git.js
@@ -1,82 +1,59 @@
 'use strict';
 
 var xProcess = require ('xcraft-core-process') ();
-var xFs      = require ('xcraft-core-fs');
-var Subst    = require ('xcraft-core-subst').Subst;
+var subst    = require ('xcraft-core-subst');
 
 exports.clone = function (uri, ref, destPath, callback) {
   var async = require ('async');
   var fs    = require ('fs');
-  var path  = require ('path');
 
-  var subst = null;
-  var mountDir = path.dirname (destPath);
-  var mountDest = '';
+  subst.wrap (destPath, function (err, dest, callback) {
+    if (err) {
+      callback (err);
+      return;
+    }
 
-  xFs.mkdir (mountDir);
+    async.series ([
+      function (callback) {
+        var args = [
+          'clone',
+          '--progress',
+          '--recursive',
+          uri,
+          dest
+        ];
 
-  async.series ([
-    function (callback) {
-      subst = new Subst (mountDir);
-      subst.mount (function (err, drive) {
-        if (err) {
-          callback (err);
+        xProcess.spawn ('git', args, {}, callback);
+      },
+
+      function (callback) {
+        if (!fs.existsSync (destPath)) {
+          callback ('nothing cloned');
           return;
         }
 
-        mountDest = path.join (drive, path.basename (destPath));
-        callback ();
-      });
-    },
+        if (!ref) {
+          ref = 'master';
+        }
 
-    function (callback) {
-      var args = [
-        'clone',
-        '--progress',
-        '--recursive',
-        uri,
-        mountDest
-      ];
+        var args = [
+          'checkout',
+          ref
+        ];
 
-      xProcess.spawn ('git', args, {}, callback);
-    },
+        xProcess.spawn ('git', args, {cwd: dest}, callback);
+      },
 
-    function (callback) {
-      if (!fs.existsSync (destPath)) {
-        callback ('nothing cloned');
-        return;
+      function (callback) {
+        var args = [
+          'submodule',
+          'update',
+          '--init',
+          '--recursive'
+        ];
+
+        xProcess.spawn ('git', args, {cwd: dest}, callback);
       }
-
-      if (!ref) {
-        ref = 'master';
-      }
-
-      var args = [
-        'checkout',
-        ref
-      ];
-
-      xProcess.spawn ('git', args, {cwd: mountDest}, callback);
-    },
-
-    function (callback) {
-      var args = [
-        'submodule',
-        'update',
-        '--init',
-        '--recursive'
-      ];
-
-      xProcess.spawn ('git', args, {cwd: mountDest}, callback);
-    },
-
-    function (callback) {
-      if (!subst) {
-        callback ();
-        return;
-      }
-
-      subst.umount (callback);
-    }
-  ], callback);
+    ], callback);
+  }, callback);
 };
